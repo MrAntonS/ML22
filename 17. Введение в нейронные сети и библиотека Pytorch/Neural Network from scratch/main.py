6f651c4d84eabde6f38afea79b811ccf50dd648f
Anton <mrantonsaenko@gmail.com>
2022-08-07 21:54:11 -0400

    added sigmoid

--- 17. Введение в нейронные сети и библиотека Pytorch/Neural Network from scratch/main.py
+++ 17. Введение в нейронные сети и библиотека Pytorch/Neural Network from scratch/main.py
@@ -9,6 +9,8 @@ class Neuron():
     def __init__(self, num_of_inputs, bias=None):
         self.w = np.random.randn(num_of_inputs)
         self.bias = np.random.randint(0,10) if bias == None else bias
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-1 * x))
     def forward(self, inputs):
-        return np.sum(inputs * self.w) + self.bias
+        return self.sigmoid(np.sum(inputs * self.w) + self.bias)
     
