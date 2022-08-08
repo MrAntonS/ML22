3369cdfc08b76a07d6bbbff4ce0f7c5dca761481
Anton <mrantonsaenko@gmail.com>
2022-08-07 22:01:48 -0400

    Got neuron forward prop working

--- 17. Введение в нейронные сети и библиотека Pytorch/Neural Network from scratch/main.py
+++ 17. Введение в нейронные сети и библиотека Pytorch/Neural Network from scratch/main.py
@@ -5,12 +5,25 @@ import matplotlib as plt
 
 np.random.seed(42)
 
+
 class Neuron():
     def __init__(self, num_of_inputs, bias=None):
         self.w = np.random.randn(num_of_inputs)
-        self.bias = np.random.randint(0,10) if bias == None else bias
+        self.bias = np.random.randint(0, 10) if bias == None else bias
+
     def _sigmoid(self, x):
         return 1 / (1 + np.exp(-1 * x))
+
+    def get_scalar(self, inputs):
+        return np.sum(inputs * self.w) + self.bias
+
     def forward(self, inputs):
-        return self.sigmoid(np.sum(inputs * self.w) + self.bias)
-    
+        return self._sigmoid(np.sum(inputs * self.w) + self.bias)
+
+    def get_weights(self):
+        return self.w
+
+
+neuron = Neuron(4, 2)
+print(neuron.get_weights())
+print(neuron.forward(np.array([1, 2, 3, 2.5])))
