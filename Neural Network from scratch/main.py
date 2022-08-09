e546cd4c7104de28683cbd73dc0522420a0b11a0
Anton <mrantonsaenko@gmail.com>
2022-08-09 15:12:54 -0400

    Rewrite

--- Neural Network from scratch/main.py
+++ Neural Network from scratch/main.py
@@ -1,4 +1,7 @@
+from asyncio.constants import SENDFILE_FALLBACK_READBUFFER_SIZE
+from os import access
 import sys
+from turtle import forward
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,67 +23,101 @@ def create_data(points, classes):
     return X, y
 
 
-class Neuron:
-    def __init__(self, num_of_inputs, bias=None):
-        self.w = 0.1 * np.random.randn(num_of_inputs)
-        self.bias = 0  # np.random.randint(0, 10) if bias == None else bias
-        self.scalar = None
+# class Neuron:
+#     def __init__(self, num_of_inputs, bias=None):
+#         self.w = 0.1 * np.random.randn(num_of_inputs)
+#         self.bias = 0  # np.random.randint(0, 10) if bias == None else bias
+#         self.scalar = None
 
-    def _sigmoid(self, x):
-        return 1 / (1 + np.exp(-1 * x))
+#     def _sigmoid(self, x):
+#         return 1 / (1 + np.exp(-1 * x))
 
-    def _relu(self, x):
-        return np.where(0 >= x, 0, x)
+#     def _relu(self, x):
+#         return np.where(0 >= x, 0, x)
 
-    def predict(self, inputs):
-        self.scalar = np.dot(inputs, self.w) + self.bias
-        return self.scalar
+#     def predict(self, inputs):
+#         self.scalar = np.dot(inputs, self.w) + self.bias
+#         return self.scalar
 
-    def forward(self, inputs):
-        return self._sigmoid(self.predict(inputs))
+#     def forward(self, inputs):
+#         return self._sigmoid(self.predict(inputs))
 
-    def forward_relu(self, inputs):
-        return self._relu(self.predict(inputs))
+#     def forward_relu(self, inputs):
+#         return self._relu(self.predict(inputs))
 
-    def get_weights(self):
-        return self.w
+#     def get_weights(self):
+#         return self.w
+
+class LossFunction:
+    def __init__(self):
+        self.result = None
+
+
+class CCELoss(LossFunction):
+    def get_loss(self, y, y_pred):
+        np.clip(y_pred, 1e-7, 1e7)
+        if len(y.shape) == 1:
+            return np.mean(-np.log(y_pred[range(len(y_pred)), y]))
+        elif len(y.shape) == 2:
+            return np.mean(-np.log(np.sum(y_pred * y, axis=1)))
+
+
+class ActivationFunction:
+    def __init__(self):
+        self.results = None
+
+
+class Sigmoid(ActivationFunction):
+    def forward(self, X):
+        self.results = 1 / (1 + np.exp(X))
+        return self.results
+
+
+class Relu(ActivationFunction):
+    def forward(self, X):
+        self.results = np.where(0 >= X, 0, X)
+        return self.results
+
+
+class Softmax(ActivationFunction):
+    def forward(self, X):
+        X = X - np.max(X, axis=1, keepdims=True)
+        exponent = np.exp(X)
+        self.results = exponent / np.sum(exponent, axis=1, keepdims=True)
+        return self.results
 
 
 class LinearLayer:
-    def __init__(self, num_of_inputs, amount_of_neurons):
-        self.neurons = np.array([Neuron(num_of_inputs)
-                                for i in range(amount_of_neurons)], dtype=np.object0)
-        self.result = None
+    def __init__(self, num_of_inputs, amount_of_neurons, activation: ActivationFunction, loss_func: LossFunction):
+        # self.neurons = np.array([Neuron(num_of_inputs)
+        #                         for i in range(amount_of_neurons)], dtype=np.object0)
+        self.weights = np.random.randn(amount_of_neurons, num_of_inputs)
+        self.bias = np.ones(amount_of_neurons)
+        self.activation = activation
+        self.loss_func = loss_func
 
     def fit(self, X, y):
-        print(self.__loss(y, self.forward_softmax(X)))
+        pass
 
-    def forward_sigmoid(self, inputs: np.ndarray):
-        self.result = np.array([i.forward(inputs)
-                                for i in self.neurons], dtype=np.float64).T
+    def predict(self, X):
+        return np.dot(X, self.weights.T) + self.bias
+
+    def forward(self, X):
+        self.result = self.activation.forward(self.predict(X))
         return self.result
 
-    def forward_relu(self, inputs: np.ndarray):
-        self.result = np.array([i.forward_relu(inputs)
-                                for i in self.neurons], dtype=np.float64).T
-        return self.result
+    def get_loss(self, y):
+        return self.loss_func.get_loss(y, self.result)
 
     def get_weights(self):
-        return np.array([i.get_weights() for i in self.neurons])
-
-    def forward_softmax(self, inputs):
-        inputs = inputs - np.max(inputs, axis=1, keepdims=True)
-        softmax_results = np.exp(inputs)
-        self.result = softmax_results / \
-            np.sum(softmax_results, axis=1, keepdims=True)
-        return self.result
-
-    def __loss(self, y, y_pred):
-        np.clip(y_pred, 1e-7, 1e7)
-        return np.mean(-np.log(y_pred[range(len(y_pred)), y-1]))
+        return self.weights
 
 
 X, y = create_data(100, 3)
-network_layer1 = LinearLayer(X.shape[1], 3)  # RELU
-network_layer2 = LinearLayer(3, 3)  # SoftMax
-network_layer2.fit(X, y)
+network_layer1 = LinearLayer(X.shape[1], 3, Sigmoid(), CCELoss())  # RELU
+network_layer2 = LinearLayer(3, 3, Softmax(), CCELoss())  # RELU
+network_layer3 = LinearLayer(3, 3, Softmax(), CCELoss())  # RELU
+network_layer1.forward(X)
+network_layer2.forward(network_layer1.result)
+network_layer3.forward(network_layer2.result)
+print(network_layer3.get_loss(y))
