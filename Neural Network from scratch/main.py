03162542c4a0b86cfc671f25ed0c106d368d0f7a
Anton <mrantonsaenko@gmail.com>
2022-08-10 23:43:56 -0400

    FINALLY SOME PROGRESS

--- Neural Network from scratch/main.py
+++ Neural Network from scratch/main.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-np.random.seed(0)
+np.random.seed(1)
 np.set_printoptions(precision=70)
+
 
 class Data_Creator:
     def create_data_spiral(self, points, classes):
@@ -27,7 +28,7 @@ Loss Functions
 
 class LossFunction:
     def __init__(self):
-        self.result = None
+        self.results = None
         self.derivative = None
 
     def forward(self, y, y_pred):
@@ -41,15 +42,13 @@ class CCELoss(LossFunction):
     def forward(self, y, y_pred):
         np.clip(y_pred, 1e-7, 1e7)
         if len(y.shape) == 1:
-            self.result = -np.log(y_pred[range(len(y_pred)), y])
+            self.results = -np.log(y_pred[range(len(y_pred)), y])
         elif len(y.shape) == 2:
-            self.result = -np.log(np.sum(y_pred * y, axis=1))
-        return self.result
+            self.results = -np.log(np.sum(y_pred * y, axis=1))
+        return self.results
 
-    def backward(self, y, y_pred):
-        self.derivative = np.tile(self.result, (y_pred.shape[1], 1)).T
-        self.derivative[range(len(y_pred)), y] = 1 / \
-            y_pred[range(len(y_pred)), y]
+    def backward(self, y_pred):
+        self.derivative = -1 / y_pred
         return self.derivative
 
 
@@ -63,10 +62,10 @@ class ActivationFunction:
         self.results = None
         self.f = None
 
-    def forward(self, input):
+    def forward(self, inputs):
         pass
 
-    def backward(self, input):
+    def backward(self, inputs):
         pass
 
 
@@ -74,12 +73,12 @@ class Sigmoid(ActivationFunction):
     def __init__(self):
         self.f = lambda x: 1 / (1 + np.exp(-x))
 
-    def forward(self, X):
-        self.results = self.f(X)
+    def forward(self, inputs):
+        self.results = self.f(inputs)
         return self.results
 
-    def backward(self, X):
-        self.derivative = X * (1 - X)
+    def backward(self, inputs):
+        self.derivative = self.f(inputs) * (1 - self.f(inputs))
         return self.derivative
 
 
@@ -87,19 +86,19 @@ class Relu(ActivationFunction):
     def __init__(self):
         self.f = lambda x: np.where(0 >= x, 0, x)
 
-    def forward(self, X):
-        self.results = self.f(X)
+    def forward(self, inputs):
+        self.results = self.f(inputs)
         return self.results
 
-    def backward(self, X):
-        self.derivative = np.where(0 >= X, 0, 1)
+    def backward(self, inputs):
+        self.derivative = np.where(0 >= inputs, 0, 1)
         return self.derivative
 
 
 class Softmax(ActivationFunction):
-    def forward(self, X):
-        X = X - np.max(X, axis=1, keepdims=True)
-        exponent = np.exp(X)
+    def forward(self, inputs):
+        inputs_norm = inputs - np.max(inputs, axis=1, keepdims=True)
+        exponent = np.exp(inputs_norm)
         self.results = exponent / np.sum(exponent, axis=1, keepdims=True)
         return self.results
 
@@ -110,20 +109,47 @@ Layers
 
 
 class LinearLayer:
-    def __init__(self, num_of_inputs, amount_of_neurons, activation: ActivationFunction, loss_func: LossFunction):
+    def __init__(self, num_of_inputs, amount_of_neurons):
         self.weights = np.random.randn(amount_of_neurons, num_of_inputs)
-        self.bias = np.ones(amount_of_neurons)
+        self.bias = np.zeros(amount_of_neurons)
+        self.results = None
+        self.gradinput = None
+        self.inputs = None
 
     def grad(self, inputs, gradoutputs):
-        self.gradinput = np.dot(gradoutputs, self.weights.T)
-        self.gradW = np.dot(gradoutputs, inputs.T)
-        self.gradB = np.sum(gradoutputs, axis=0)
         return self.gradinput
 
-    def forward(self, X):
-        self.raw_predict = np.dot(X, self.weights.T) + self.bias
-        return self.raw_predict
+    def forward(self, inputs):
+        self.inputs = inputs.copy()
+        self.results = np.dot(inputs, self.weights.T) + self.bias
+        return self.results
+
     def backward(self, inputs, gradoutputs):
         pass
+
     def get_weights(self):
         return self.weights
+
+
+X = np.array([[-1, -2]])
+y = np.array([1])
+Linear_layer = LinearLayer(2, 2)
+Linear_layer.weights = np.array([[1,1], [2, 2]])
+for _ in range(100):
+    Linear_layer.forward(X)
+    Sigmoid_layer = Sigmoid()
+    Sigmoid_layer.forward(Linear_layer.results)
+    Loss = CCELoss()
+    Loss.forward(y, Sigmoid_layer.results)
+    # print(Linear_layer.weights.shape)
+    # print(Linear_layer.bias.shape)
+    # print(Loss.results)
+    # print(Loss.backward(Sigmoid_layer.results))
+    # print(Sigmoid_layer.backward(Linear_layer.results))
+    gradout = (Loss.backward(Sigmoid_layer.results) * Sigmoid_layer.backward(Linear_layer.results)).T
+    gradW = gradout @ Linear_layer.inputs
+    gradInput = Linear_layer.weights.T @ gradout
+    gradB = gradout
+    Linear_layer.weights = Linear_layer.weights - 0.01 * gradW
+    Linear_layer.bias = Linear_layer.bias - gradB.T
+    print(Loss.results)
